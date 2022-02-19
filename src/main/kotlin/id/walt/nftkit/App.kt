f05282106a382d2e38be6e26a22081ffdea407db
Houcem <walidelkhemiri@gmail.com>
2022-02-19 13:38:34 +0100

    EVM blockchain: send transaction and read data

--- src/main/kotlin/id/walt/nftkit/App.kt
+++ src/main/kotlin/id/walt/nftkit/App.kt
@@ -1,7 +1,14 @@
 package id.walt.nftkit
 
+import org.web3j.abi.datatypes.Address
+import org.web3j.abi.datatypes.Utf8String
+import org.web3j.abi.datatypes.generated.Uint256
+import org.web3j.crypto.Credentials
 import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.methods.response.TransactionReceipt
 import org.web3j.protocol.http.HttpService
+import org.web3j.tx.gas.ContractGasProvider
+import java.math.BigInteger
 
 
 class App {
@@ -14,6 +21,18 @@ class App {
 fun main() {
     println(App().greeting)
 
-    val web3j = Web3j.build(HttpService("<your_node_url>"))
+    val web3j = Web3j.build(HttpService("https://ropsten.infura.io/v3/0184192d0f2942c3b0322d79eca162b2"))//0xc831de165bD2356230e60DF549324034dB5A3BD5
+    val credentials: Credentials = Credentials.create("d720ef2cb49c6cbe94175ed413d27e635c5acaa1b7cf03d1faad3a0abc2f53f3")
+    println("Account address: " + credentials.address);
+    val gasProvider: ContractGasProvider = WaltIdGasProvider
+    val credentialNFT : CredentialNFT = CredentialNFT.load("0xc831de165bD2356230e60DF549324034dB5A3BD5", web3j, credentials, gasProvider)
+    val address : Address = Address("0x2555e3a97c4ac9705D70b9e5B9b6cc6Fe2977A74")
+    System.out.println(credentialNFT.balanceOf(address).send().value)
+    val tokenuri = Utf8String("test")
+    val receipt: TransactionReceipt = credentialNFT.mintTo(address, tokenuri).send()
+    println(receipt.transactionHash)
+    println(credentialNFT.balanceOf(address).send().value)
+    val tokenuriValue = credentialNFT.tokenURI(Uint256(BigInteger.valueOf(2))).send().value
+    println(tokenuriValue)
 
 }
---
+++ src/main/solidity/CredentialNFT.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract CredentialNFT is ERC721URIStorage , Ownable  {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+  constructor() ERC721("VerifiableCredential", "VC") {
+  }
+
+  //The recipient is the subject of the VC
+     function mintTo(address _recipient, string memory tokenURI) public  returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(_recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+      require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
+      string memory _tokenURI = super.tokenURI(tokenId);
+      return string(abi.encodePacked("data:application/json;base64,",_tokenURI));
+   } 
+
+   /**
+     * @dev See {IERC721-approve}.
+     */
+    function approve(address to, uint256 tokenId) public virtual override {
+        revert("Nontransferable token");
+    }
+
+     /**
+     * @dev See {IERC721-safeTransferFrom}.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {
+        revert("Nontransferable token");
+    }
+
+    /**
+     * @dev See {IERC721-safeTransferFrom}.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public virtual override {
+        revert("Nontransferable token");
+    }
+
+     /**
+     * @dev Approve `operator` to operate on all of `owner` tokens
+     *
+     * Emits a {ApprovalForAll} event.
+     */
+    function _setApprovalForAll(
+        address owner,
+        address operator,
+        bool approved
+    ) internal virtual override{
+        revert("Nontransferable token");
+    }
+
+      /**
+     * @dev See {IERC721-transferFrom}.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {
+        revert("Nontransferable token");
+    }
+
+}
