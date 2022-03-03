753011086e9302c8bd15c6d70d9110f64bb602cf
walid <walidelkhemiri@gmail.com>
2022-03-03 09:53:46 +0100

    set first version of service layer interface

--- src/main/kotlin/id/walt/nftkit/utilis/BasicWeb3Instance.kt
+++ src/main/kotlin/id/walt/nftkit/utilis/BasicWeb3Instance.kt
@@ -5,6 +5,6 @@ import org.web3j.protocol.http.HttpService
 
 class BasicWeb3Instance : Web3jInstance {
     override fun getWeb3j(): Web3j {
-        return Web3j.build(HttpService(""))
+        return Web3j.build(HttpService("https://ropsten.infura.io/v3/868f60c9c4d149fe9dbd1c0acd378ae4"))
     }
 }
