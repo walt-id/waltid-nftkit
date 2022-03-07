dd3afcf35b8ee0d9759964c062f26d1b22c6f3f8
walid <walidelkhemiri@gmail.com>
2022-03-07 11:18:08 +0100

    Add chain as parameter

--- src/main/kotlin/id/walt/nftkit/chains/evm/erc721/Erc721TokenStandard.kt
+++ src/main/kotlin/id/walt/nftkit/chains/evm/erc721/Erc721TokenStandard.kt
@@ -1,8 +1,9 @@
 package id.walt.nftkit.chains.evm.erc721
 
 import id.walt.nftkit.WaltIdGasProvider
+import id.walt.nftkit.services.Chain
 import id.walt.nftkit.smart_contract_wrapper.Erc721OnchainCredentialWrapper
-import id.walt.nftkit.utilis.BasicWeb3Instance
+import id.walt.nftkit.utilis.RopstenWeb3Instance
 import org.web3j.abi.datatypes.Address
 import org.web3j.abi.datatypes.Utf8String
 import org.web3j.abi.datatypes.generated.Uint256
@@ -14,38 +15,38 @@ import java.math.BigInteger
 class Erc721TokenStandard : IErc721TokenStandard {
 
 
-    override fun mintToken(contractAddress: String, recipient: Address, tokenURI: Utf8String): TransactionReceipt? {
-        val erc721OnchainCredentialWrapper = loadContract(contractAddress)
+    override fun mintToken(chain: Chain, contractAddress: String, recipient: Address, tokenURI: Utf8String): TransactionReceipt? {
+        val erc721OnchainCredentialWrapper = loadContract(chain, contractAddress)
         return erc721OnchainCredentialWrapper.mintTo(recipient, tokenURI).send()
     }
 
-    override fun ownerOf(contractAddress: String, tokenId: Uint256): String? {
-        val erc721OnchainCredentialWrapper = loadContract(contractAddress)
+    override fun ownerOf(chain: Chain, contractAddress: String, tokenId: Uint256): String? {
+        val erc721OnchainCredentialWrapper = loadContract(chain, contractAddress)
         return erc721OnchainCredentialWrapper.ownerOf(tokenId).send().value
     }
 
-    override fun name(contractAddress: String): String {
-        val erc721OnchainCredentialWrapper = loadContract(contractAddress)
+    override fun name(chain: Chain, contractAddress: String): String {
+        val erc721OnchainCredentialWrapper = loadContract(chain, contractAddress)
         return erc721OnchainCredentialWrapper.name().send().value
     }
 
-    override fun symbol(contractAddress: String): String {
-        val erc721OnchainCredentialWrapper = loadContract(contractAddress)
+    override fun symbol(chain: Chain, contractAddress: String): String {
+        val erc721OnchainCredentialWrapper = loadContract(chain, contractAddress)
         return erc721OnchainCredentialWrapper.symbol().send().value
     }
 
-    override fun tokenURI(contractAddress: String, tokenId: Uint256): String? {
-        val erc721OnchainCredentialWrapper = loadContract(contractAddress)
+    override fun tokenURI(chain: Chain, contractAddress: String, tokenId: Uint256): String? {
+        val erc721OnchainCredentialWrapper = loadContract(chain, contractAddress)
         return erc721OnchainCredentialWrapper.tokenURI(tokenId).send().value
     }
 
-    override fun balanceOf(contractAddress: String, owner: Address): BigInteger? {
-        val erc721OnchainCredentialWrapper = loadContract(contractAddress)
+    override fun balanceOf(chain: Chain, contractAddress: String, owner: Address): BigInteger? {
+        val erc721OnchainCredentialWrapper = loadContract(chain, contractAddress)
         return erc721OnchainCredentialWrapper.balanceOf(owner).send().value
     }
 
-    private fun loadContract(address: String) : Erc721OnchainCredentialWrapper{
-        val basicWeb3Instance : BasicWeb3Instance = BasicWeb3Instance()
+    private fun loadContract(chain: Chain, address: String) : Erc721OnchainCredentialWrapper{
+        val basicWeb3Instance  = RopstenWeb3Instance()
         val web3j = basicWeb3Instance.getWeb3j()
 
         val credentials: Credentials = Credentials.create("d720ef2cb49c6cbe94175ed413d27e635c5acaa1b7cf03d1faad3a0abc2f53f3")
--- src/main/kotlin/id/walt/nftkit/utilis/RopstenWeb3Instance.kt
+++ src/main/kotlin/id/walt/nftkit/utilis/RopstenWeb3Instance.kt
@@ -3,7 +3,7 @@ package id.walt.nftkit.utilis
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.http.HttpService
 
-class BasicWeb3Instance : Web3jInstance {
+class RopstenWeb3Instance : Web3jInstance {
     override fun getWeb3j(): Web3j {
         return Web3j.build(HttpService("https://ropsten.infura.io/v3/868f60c9c4d149fe9dbd1c0acd378ae4"))
     }
