0966e14c63afe7a350f28a9de8e4537ef98c855d
walid <walidelkhemiri@gmail.com>
2022-03-18 16:33:42 +0100

    Add config-file support with hoplite + Refactoring the get provider code

---
+++ src/main/kotlin/id/walt/nftkit/utilis/providers/ProviderFactory.kt
@@ -0,0 +1,15 @@
+package id.walt.nftkit.utilis.providers
+
+import id.walt.nftkit.services.Chain
+
+object ProviderFactory {
+
+    fun getProvider(chain: Chain): Web3jInstance? = when(chain){
+        Chain.ETHEREUM -> EthereumWeb3()
+        Chain.ROPSTEN -> RopstenWeb3()
+        Chain.RINKEBY -> RinkebyWeb3()
+        Chain.POLYGON -> PolygonWeb3()
+        else -> null
+    }
+}
+
