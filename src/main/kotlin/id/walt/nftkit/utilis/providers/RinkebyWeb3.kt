8e62a47eee03e105f7033f0c2387bb9196883191
walid <walidelkhemiri@gmail.com>
2022-03-18 16:33:42 +0100

    Add config-file support with hoplite + Refactoring the get provider code

---
+++ src/main/kotlin/id/walt/nftkit/utilis/providers/RinkebyWeb3.kt
@@ -0,0 +1,11 @@
+package id.walt.nftkit.utilis.providers
+
+import id.walt.nftkit.services.WaltIdServices
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.http.HttpService
+
+class RinkebyWeb3: Web3jInstance {
+    override fun getWeb3j(): Web3j {
+        return Web3j.build(HttpService(WaltIdServices.loadProvidersConfig().providers.rinkeby))
+    }
+}
