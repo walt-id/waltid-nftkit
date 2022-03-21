20ba5207c2bfa7e960e37b6c20ee64f9f1013293
walid <walidelkhemiri@gmail.com>
2022-03-21 09:46:32 +0100

    Polygon support

--- src/main/kotlin/id/walt/nftkit/chains/evm/erc721/Erc721TokenStandard.kt
+++ src/main/kotlin/id/walt/nftkit/chains/evm/erc721/Erc721TokenStandard.kt
@@ -6,13 +6,14 @@ import id.walt.nftkit.services.DeploymentResponse
 import id.walt.nftkit.services.TransactionResponse
 import id.walt.nftkit.smart_contract_wrapper.ERC721URIStorage
 import id.walt.nftkit.utilis.providers.ProviderFactory
-import id.walt.nftkit.utilis.providers.RopstenWeb3
 import org.web3j.abi.datatypes.Address
 import org.web3j.abi.datatypes.Utf8String
 import org.web3j.abi.datatypes.generated.Bytes4
 import org.web3j.abi.datatypes.generated.Uint256
 import org.web3j.crypto.Credentials
 import org.web3j.protocol.core.methods.response.TransactionReceipt
+import org.web3j.tx.RawTransactionManager
+import org.web3j.tx.TransactionManager
 import org.web3j.tx.gas.ContractGasProvider
 import org.web3j.utils.Numeric
 import java.math.BigInteger
@@ -23,9 +24,14 @@ object Erc721TokenStandard : IErc721TokenStandard {
 
     fun deployContract(chain: Chain, name: String, symbol: String): DeploymentResponse {
         val web3j = ProviderFactory.getProvider(chain)?.getWeb3j()
-        val credentials: Credentials = Credentials.create("d720ef2cb49c6cbe94175ed413d27e635c5acaa1b7cf03d1faad3a0abc2f53f3")
+        val credentials: Credentials = Credentials.create("bd4cb3e507f342ee3a710370cef39dda48f17b0a158b0b8dd3f000fbd5b2c2d9")
         val gasProvider: ContractGasProvider = WaltIdGasProvider
-        val contract= ERC721URIStorage.deploy(web3j,credentials,gasProvider,Utf8String(name),Utf8String(symbol)).send()
+        val transactionManager: TransactionManager = RawTransactionManager(
+            web3j, credentials, 80001
+        )
+        //val contract= ERC721URIStorage.deploy(web3j,credentials,gasProvider,Utf8String(name),Utf8String(symbol)).send()
+        val contract= ERC721URIStorage.deploy(web3j,transactionManager,gasProvider,Utf8String(name),Utf8String(symbol)).send()
+
         val ts = TransactionResponse(contract.transactionReceipt.get().transactionHash,"https://ropsten.etherscan.io/tx/"+ contract.transactionReceipt.get().transactionHash)
         return DeploymentResponse(ts, contract.contractAddress, "https://ropsten.etherscan.io/token/"+ contract.contractAddress)
 
@@ -71,11 +77,17 @@ object Erc721TokenStandard : IErc721TokenStandard {
     private fun loadContract(chain: Chain, address: String) : ERC721URIStorage{
         val web3j = ProviderFactory.getProvider(chain)?.getWeb3j()
 
-        val credentials: Credentials = Credentials.create("d720ef2cb49c6cbe94175ed413d27e635c5acaa1b7cf03d1faad3a0abc2f53f3")
+        //val credentials: Credentials = Credentials.create("d720ef2cb49c6cbe94175ed413d27e635c5acaa1b7cf03d1faad3a0abc2f53f3")
+        val credentials: Credentials = Credentials.create("bd4cb3e507f342ee3a710370cef39dda48f17b0a158b0b8dd3f000fbd5b2c2d9")
+
 
         val gasProvider: ContractGasProvider = WaltIdGasProvider
 
-        return ERC721URIStorage.load(address, web3j, credentials, gasProvider)
+        val transactionManager: TransactionManager = RawTransactionManager(
+            web3j, credentials, 80001
+        )
+
+        return ERC721URIStorage.load(address, web3j, transactionManager, gasProvider)
 
     }
 
