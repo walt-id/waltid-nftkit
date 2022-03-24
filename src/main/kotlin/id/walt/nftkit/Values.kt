6054bbe5f4a6111f49ec6fe2bee8255af2fe4a6a
walid <walidelkhemiri@gmail.com>
2022-03-24 08:41:44 +0100

    Fix redirect URLs in rest APIs response to be aligned with the selected chain

--- src/main/kotlin/id/walt/nftkit/Values.kt
+++ src/main/kotlin/id/walt/nftkit/Values.kt
@@ -7,8 +7,15 @@ object Values {
     const val POLYGON_MAINNET_CHAIN_ID: Long = 137
     const val POLYGON_TESTNET_MUMBAI_CHAIN_ID: Long = 80001
 
-    const val POLYGON_MAINNET_SCAN_URL = "api.polygonscan.com"
-    const val POLYGON_TESTNET_MUMBAI_SCAN_URL = "api-testnet.polygonscan.com"
-    const val ETHEREUM_TESTNET_RINKEBY_SCAN_URL = "api-rinkeby.etherscan.io";
+    const val ETHEREUM_TESTNET_RINKEBY_SCAN_API_URL = "api-rinkeby.etherscan.io";
+    const val POLYGON_MAINNET_SCAN_API_URL = "api.polygonscan.com"
+    const val POLYGON_TESTNET_MUMBAI_SCAN_API_URL = "api-testnet.polygonscan.com"
+
+    const val ETHEREUM_MAINNET_BLOCK_EXPLORER_URL = "https://etherscan.io"
+    const val ETHEREUM_TESTNET_RINKEBY_BLOCK_EXPLORER_URL = "https://rinkeby.etherscan.io"
+    const val ETHEREUM_TESTNET_ROPSTEN_BLOCK_EXPLORER_URL = "https://ropsten.etherscan.io"
+    const val POLYGON_MAINNET_BLOCK_EXPLORER_URL = "https://polygonscan.com"
+    const val POLYGON_TESTNET_MUMBAI_BLOCK_EXPLORER_URL = "https://mumbai.polygonscan.com"
+
 
 }
--- src/main/kotlin/id/walt/nftkit/services/WaltIdServices.kt
+++ src/main/kotlin/id/walt/nftkit/services/WaltIdServices.kt
@@ -4,6 +4,7 @@ import com.sksamuel.hoplite.ConfigLoader
 import com.sksamuel.hoplite.PropertySource
 import com.sksamuel.hoplite.hikari.HikariDataSourceDecoder
 import com.sksamuel.hoplite.yaml.YamlParser
+import id.walt.nftkit.Values
 import java.io.File
 import java.util.*
 
@@ -11,8 +12,8 @@ import java.util.*
 data class Providers(val ethereum: String, val rinkeby: String, val ropsten: String, val polygon:String, val mumbai: String)
 data class ProvidersConfig(val providers: Providers)
 
-data class ChainScanApiKeys(val ethereum: String, val polygon:String)
-data class ChainScanApiKeyConfig(val chainScanApiKeys: ChainScanApiKeys)
+data class BlockExplorerScanApiKeys(val ethereum: String, val polygon:String)
+data class BlockExplorerScanApiKeyConfig(val blockExplorerScanApiKeys: BlockExplorerScanApiKeys)
 
 object WaltIdServices {
 
@@ -36,5 +37,15 @@ object WaltIdServices {
         .addSource(PropertySource.resource("/walt-default.yaml"))
         .addDecoder(HikariDataSourceDecoder())
         .build()
-        .loadConfigOrThrow<ChainScanApiKeyConfig>()
+        .loadConfigOrThrow<BlockExplorerScanApiKeyConfig>()
+
+    fun getBlockExplorerUrl(chain: Chain): String {
+        return when(chain){
+            Chain.ETHEREUM -> Values.ETHEREUM_MAINNET_BLOCK_EXPLORER_URL
+            Chain.RINKEBY -> Values.ETHEREUM_TESTNET_RINKEBY_BLOCK_EXPLORER_URL
+            Chain.ROPSTEN -> Values.ETHEREUM_TESTNET_ROPSTEN_BLOCK_EXPLORER_URL
+            Chain.POLYGON -> Values.POLYGON_MAINNET_BLOCK_EXPLORER_URL
+            Chain.MUMBAI -> Values.POLYGON_TESTNET_MUMBAI_BLOCK_EXPLORER_URL
+        }
+    }
 }
