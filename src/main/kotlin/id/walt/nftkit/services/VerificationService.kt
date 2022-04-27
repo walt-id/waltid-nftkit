1538f290f941db0de875321f1e439827982dfd45
walid <walidelkhemiri@gmail.com>
2022-04-27 16:54:28 +0100

    Ownership and traits verification

--- src/main/kotlin/id/walt/nftkit/services/VerificationService.kt
+++ src/main/kotlin/id/walt/nftkit/services/VerificationService.kt
@@ -5,6 +5,7 @@ import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
+import org.web3j.tx.exceptions.ContractCallException
 import java.math.BigInteger
 
 
@@ -20,34 +21,33 @@ object VerificationService {
     }
 
     // Verify if NFT is part of a collection (contract address)
-    fun  verifyCollection(chain: Chain, contractAddress: String, accountAddress: String): Boolean{
-        val accountNfts = getAccountNftsPerContract(chain, contractAddress, accountAddress)
-        if(accountNfts.size > 0){
-            return true
-        }
-        return false
+    fun  verifyCollection(chain: Chain, contractAddress: String, account: String, tokenId: String): Boolean{
+        return ownershipVerification(chain, contractAddress, account, BigInteger(tokenId))
     }
 
     //  simply check if a certain trait type and trait value is in the metadata
-    fun verifyTrait(chain: Chain, contractAddress: String, accountAddress: String, traitType: String, traitValue: String? = null): Boolean {
-        val accountNfts = getAccountNftsPerContract(chain, contractAddress, accountAddress)
-
-        if(accountNfts.size > 0){
-            accountNfts.forEach{
-                val nftMetadata = NftService.getNftMetadata(chain, it!!.contractAddress, BigInteger( it?.tokenID))
-                if(nftMetadata.attributes?.filter { (it.trait_type.equals(traitType) && it.value.equals(traitValue)) || (traitValue == null && traitType.equals(it.trait_type) ) }!!.size > 0){
-                    return true
-                }
+    fun verifyTrait(chain: Chain, contractAddress: String, account: String, tokenId: String, traitType: String, traitValue: String? = null): Boolean {
+        val ownership= ownershipVerification(chain, contractAddress, account, BigInteger(tokenId))
+        if(ownership == true){
+            val metadata= NftService.getNftMetadata(chain, contractAddress, BigInteger( tokenId))
+            if(metadata.attributes?.filter { (it.trait_type.equals(traitType) && it.value.equals(traitValue, true)) || (traitValue == null && traitType.equals(it.trait_type) ) }!!.size > 0){
+                return true
             }
         }
         return false
     }
 
-    private fun getAccountNftsPerContract(chain: Chain, contractAddress: String, accountAddress: String): List<Token?> {
-        val nfts = NftService.getAccountNFTsByChainScan(chain, accountAddress)
-        return nfts.filter { it?.contractAddress.equals(contractAddress, ignoreCase = true) }
+    private fun ownershipVerification(chain: Chain, contractAddress: String, account: String, tokenId: BigInteger): Boolean{
+        try {
+            val owner= NftService.ownerOf(chain, contractAddress, tokenId)
+            if(account.equals(owner, true)){
+                return true
+            }
+            return false
+        }catch (e: ContractCallException){
+            return false
+        }
+        return false
     }
-
-
 
 }
