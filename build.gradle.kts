9f3c159c845af1478b134d63593e93b657f7e04b
Philipp Potisk <phil@walt.id>
2022-05-05 15:22:31 +0200

    Set to public maven-repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,7 +89,7 @@ publishing {
 
     repositories {
         maven {
-            url = uri("https://maven.pkg.github.com/walt-id/waltid-nftkit")
+            url = uri("https://maven.walt.id/repository/waltid")
             val usernameFile = File("secret_maven_username.txt")
             val passwordFile = File("secret_maven_password.txt")
             val secretMavenUsername = System.getenv()["MAVEN_USERNAME"] ?: if (usernameFile.isFile) { usernameFile.readLines()[0] } else { "" }
