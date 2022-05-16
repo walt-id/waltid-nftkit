3b9d847a6dd88846e8490f8d741993b7b646fef1
Philipp Potisk <phil@walt.id>
2022-05-16 18:15:47 +0200

    Updated gas-price

--- src/main/kotlin/id/walt/nftkit/utilis/WaltIdGasProvider.kt
+++ src/main/kotlin/id/walt/nftkit/utilis/WaltIdGasProvider.kt
@@ -2,7 +2,14 @@ package id.walt.nftkit
 
 import org.web3j.tx.gas.StaticGasProvider
 import java.math.BigInteger
+// 1 MATIC => 1.000.000.000 Gwei
+// 1 Gwei =>  1.000.000.000 Wei
 
-object WaltIdGasProvider : StaticGasProvider(BigInteger.valueOf(60_000_000_000L), BigInteger.valueOf(4_498_868)) {
+// Gas (16th Mai 2022): 0.68$
+// - https://cointool.app/gasPrice/matic
+// - https://polygonscan.com/address/0xb789711a8cf2a3938779d01866a4c376598b9fe6
+
+// Config Gas: 80 Wei
+object WaltIdGasProvider : StaticGasProvider(BigInteger.valueOf(100_000_000_000L), BigInteger.valueOf(4_498_868)) {
 
 }
