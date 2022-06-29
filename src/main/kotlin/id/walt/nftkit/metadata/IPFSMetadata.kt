1729c33a1da40f3bbdd74b2085849522cbcbec2c
walid <walidelkhemiri@gmail.com>
2022-06-29 09:47:09 +0100

    fix api key nft.stoarge problem

--- src/main/kotlin/id/walt/nftkit/metadata/IPFSMetadata.kt
+++ src/main/kotlin/id/walt/nftkit/metadata/IPFSMetadata.kt
@@ -2,9 +2,17 @@ package id.walt.nftkit.metadata
 
 import id.walt.nftkit.services.NftMetadata
 import id.walt.nftkit.services.NftService
+import id.walt.nftkit.services.WaltIdServices
+import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.forms.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
@@ -32,11 +40,34 @@ data class NFTStorageAddFileResult(
     )
 }
 
+
 object IPFSMetadata: MetadataUri {
+
+    val client = HttpClient(CIO.create{requestTimeout = 0}) {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+            })
+        }
+        install(Auth) {
+            bearer {
+                loadTokens {
+                    val token = WaltIdServices.loadApiKeys().apiKeys.nftstorage
+                    BearerTokens(token, token)
+                }
+            }
+        }
+        install(Logging) {
+            logger = Logger.SIMPLE
+            level = LogLevel.ALL
+            // LogLevel.BODY
+        }
+        expectSuccess = false
+    }
     override fun getTokenUri(nftMetadata: NftMetadata?): String {
         return runBlocking {
             val metadata= Json.encodeToString(nftMetadata)
-            val result = NftService.client.submitForm(url = "https://api.nft.storage/store", formParameters = Parameters.build {
+            val result = client.submitForm(url = "https://api.nft.storage/store", formParameters = Parameters.build {
                 append("meta", metadata)
             }).body<NFTStorageResult>()
             if(result.ok == true){
