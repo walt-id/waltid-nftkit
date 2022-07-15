3e6320840eb83a2ff93192784cadf72d50b7c142
ahmedsmiai <47064156+ahmedsmiai@users.noreply.github.com>
2022-07-15 11:09:16 +0100

    fixed integration tests

--- src/test/kotlin/id/walt/nftkit/erc721Features/Erc721StandardTest.kt
+++ src/test/kotlin/id/walt/nftkit/erc721Features/Erc721StandardTest.kt
@@ -7,10 +7,14 @@ import io.kotest.matchers.shouldNotBe
 import java.math.BigInteger
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Erc721StandardTest : StringSpec({
 
     val enableTest = false
+
+
 
     "Smart contract deployment".config(enabled = enableTest) {
         val deploymentOptions = DeploymentOptions(AccessControl.OWNABLE, TokenStandard.ERC721)
@@ -21,21 +25,26 @@ class Erc721StandardTest : StringSpec({
     }
 
     "minting token".config(enabled=enableTest){
-        val nftMetaData = NftMetadata("string", "string", "string", "string","string")
-        val mintingParametre = MintingParameter(metadataUri = "string", recipientAddress="0x59Fbfc9ad3E3b99ae8C480590908f09019667181", metadata = nftMetaData )
+        val nftMetaData = NftMetadata("Ticket 2 description", "Ticket 2", "string", "string","string")
+        val mintingParametre = MintingParameter(metadataUri = "", recipientAddress="0xaf87c5ce7a1fb6bd5aadb6dd9c0b8ef51ef1bc31", metadata = nftMetaData )
         val mintingOption = MintingOptions(MetadataStorageType.ON_CHAIN)
         val result = NftService.mintToken(Chain.MUMBAI, contractAddress = "0xf277BE034881eE38A9b270E5b6C5c6f333Af2517",mintingParametre, mintingOption)
-        println(result.transactionResponse)
+
+        val newNFT = NftService.getNftMetadata(Chain.MUMBAI, "0xf277BE034881eE38A9b270E5b6C5c6f333Af2517", result.tokenId!!)
+
         result.tokenId shouldNotBe null
+        newNFT shouldBe nftMetaData
     }
 
-    "get nft metadatatest".config(enabled=enableTest){
-        val tokenid = BigInteger.valueOf(4)
 
+    "get nft metadata test".config(enabled=enableTest){
+        val tokenid = BigInteger.valueOf(3)
         val result = NftService.getNftMetadata(Chain.MUMBAI,
             "0xf277BE034881eE38A9b270E5b6C5c6f333Af2517", tokenid )
 
-        result shouldNotBe null
+      result.name shouldBe "Ticket #1"
+        result.description shouldBe "Ticket #1 Description"
+
     }
 
     "get metadata uri".config(enabled=enableTest){
@@ -76,18 +85,25 @@ class Erc721StandardTest : StringSpec({
         result.size shouldNotBe null
     }
 
-    "update meta data".config(enabled=enableTest){
+    "update metadata".config(enabled=enableTest){
+
+        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val currentDate = sdf.format(Date())
+
         val result = NftService.updateMetadata(chain=Chain.MUMBAI,
             contractAddress = "0xf277BE034881eE38A9b270E5b6C5c6f333Af2517",
-            tokenId="4",
-            key= "string",
-            value= "string",
+            tokenId="5",
+            key= "t1",
+            value= currentDate,
             signedAccount = null
             )
+
+        val check = NftService.getNftMetadata(chain=Chain.MUMBAI, contractAddress = "0xf277BE034881eE38A9b270E5b6C5c6f333Af2517", tokenId=BigInteger.valueOf(5))
+        check.attributes!![0].value shouldBe currentDate
         result.transactionId shouldNotBe null
     }
 
-    "get IPFS meta data sing NFTs storage".config(enabled=true){
+    "get IPFS meta data using NFTs storage".config(enabled=true){
         val tokenid = BigInteger.valueOf(10)
         var uri = NftService.getNftMetadataUri(Chain.MUMBAI,"0xf277BE034881eE38A9b270E5b6C5c6f333Af2517" ,tokenid)
         val result = NftService.getIPFSMetadataUsingNFTStorage(uri)
@@ -97,15 +113,15 @@ class Erc721StandardTest : StringSpec({
         result.image shouldBe "string"
         result.external_url shouldBe "string"
         result.attributes?.get(0)!!.trait_type shouldBe "string"
-        result.attributes?.get(0)!!.value shouldBe "string"
-
+        result.attributes?.get(0)!!.value shouldBe "15/7/2022 10:30:07"
     }
 
-    "add file to ipfs nft storage".config(enabled=enableTest){
-        val byteArray = Files.readAllBytes(Paths.get("C:\\Users\\ahmed\\Desktop\\sa7eHXGY_400x400.jpg"))
+    "add file to ipfs nft storage".config(enabled=true){
+        val byteArray = Files.readAllBytes(Paths.get("src/test/resources/nft-sample/image.jpg"))
         val result = NftService.addFileToIpfsUsingNFTStorage(byteArray)
+        println(result.value.cid)
+        result.value.cid shouldNotBe null
         result.ok shouldBe true
-
     }
 
 
