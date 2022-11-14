e98d18173633b6491b59c1de98d04f571d75ec60
khemiriwalid <walidelkhemiri@gmail.com>
2022-11-14 13:56:03 +0100

    Delete js/dist directory

--- js/dist/tezos/dto/deploy.contract.dto.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVwbG95LmNvbnRyYWN0LmR0by5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3Rlem9zL2R0by9kZXBsb3kuY29udHJhY3QuZHRvLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
