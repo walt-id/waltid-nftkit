6e6655aaf7fdccd37a06e78818cba5a5c8163ecd
walid <walidelkhemiri@gmail.com>
2022-11-21 20:19:13 +0100

    update Ethereum test networks

---
+++ js/dist/tezos/dto/mint.token.dto.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWludC50b2tlbi5kdG8uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi90ZXpvcy9kdG8vbWludC50b2tlbi5kdG8udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
--- src/main/kotlin/id/walt/nftkit/Values.kt
+++ src/main/kotlin/id/walt/nftkit/Values.kt
@@ -8,20 +8,17 @@ object Values {
     const val POLYGON_TESTNET_MUMBAI_CHAIN_ID: Long = 80001
 
     const val ETHEREUM_MAINNET_SCAN_API_URL= "api.etherscan.io"
-    const val ETHEREUM_TESTNET_RINKEBY_SCAN_API_URL = "api-rinkeby.etherscan.io"
-    const val ETHEREUM_TESTNET_ROPSTEN_SCAN_API_URL = "api-ropsten.etherscan.io"
+    const val ETHEREUM_TESTNET_GOERLI_SCAN_API_URL = "api-goerli.etherscan.io"
     const val POLYGON_MAINNET_SCAN_API_URL = "api.polygonscan.com"
     const val POLYGON_TESTNET_MUMBAI_SCAN_API_URL = "api-testnet.polygonscan.com"
 
     const val ETHEREUM_MAINNET_BLOCK_EXPLORER_URL = "https://etherscan.io"
-    const val ETHEREUM_TESTNET_RINKEBY_BLOCK_EXPLORER_URL = "https://rinkeby.etherscan.io"
-    const val ETHEREUM_TESTNET_ROPSTEN_BLOCK_EXPLORER_URL = "https://ropsten.etherscan.io"
+    const val ETHEREUM_TESTNET_GOERLI_BLOCK_EXPLORER_URL = "https://goerli.etherscan.io/"
     const val POLYGON_MAINNET_BLOCK_EXPLORER_URL = "https://polygonscan.com"
     const val POLYGON_TESTNET_MUMBAI_BLOCK_EXPLORER_URL = "https://mumbai.polygonscan.com"
 
     const val ETHEREUM_MAINNET_ALCHEMY_URL = "https://eth-mainnet.alchemyapi.io/v2/"
-    const val ETHEREUM_TESTNET_RINKEBY_ALCHEMY_URL = "https://eth-rinkeby.alchemyapi.io/v2/"
-    const val ETHEREUM_TESTNET_ROPSTEN_ALCHEMY_URL = "https://eth-ropsten.alchemyapi.io/v2/"
+    const val ETHEREUM_TESTNET_GOERLI_ALCHEMY_URL = "https://eth-goerli.g.alchemy.com/v2/"
     const val POLYGON_MAINNET_ALCHEMY_URL = "https://polygon-mainnet.g.alchemy.com/v2/"
     const val POLYGON_TESTNET_MUMBAI_ALCHEMY_URL = "https://polygon-mumbai.g.alchemy.com/v2/"
 
