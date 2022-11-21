6f03b79b68c756b14aa714c598aafa98b329d770
khemiriwalid <walidelkhemiri@gmail.com>
2022-11-21 20:22:31 +0100

    Delete js directory

--- js/dist/tezos/dto/mint.token.dto.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWludC50b2tlbi5kdG8uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi90ZXpvcy9kdG8vbWludC50b2tlbi5kdG8udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
