1ff56980fea5abee5bd9691b9b3a09077d1a0b41
ahmed smiai <ahmedsmiai3@gmail.com>
2023-01-01 14:38:34 +0100

    fixed critical errors

--- src/main/kotlin/id/walt/nftkit/rest/NftKitApi.kt
+++ src/main/kotlin/id/walt/nftkit/rest/NftKitApi.kt
@@ -36,6 +36,7 @@ object NftKitApi {
 
     internal const val DEFAULT_BIND_ADDRESS = "0.0.0.0"
 
+    const val title = "walt.id NFT Kit API"
     /**
      * Currently used instance of the NFT Kit API server
      */
@@ -61,7 +62,7 @@ object NftKitApi {
                 registerPlugin(OpenApiPlugin(OpenApiOptions(InitialConfigurationCreator {
                     OpenAPI().apply {
                         info {
-                            title = "walt.id NFT Kit API"
+                            title = title
                             description = "The walt.id public API documentation"
                             contact = Contact().apply {
                                 name = "walt.id"
@@ -92,8 +93,8 @@ object NftKitApi {
                     }
                 }).apply {
                     path("/v1/nftkit/api-documentation")
-                    swagger(SwaggerOptions("/v1/nftkit/swagger").title("walt.id NFT Kit API"))
-                    reDoc(ReDocOptions("/v1/nftkit/redoc").title("walt.id NFT Kit API"))
+                    swagger(SwaggerOptions("/v1/nftkit/swagger").title(title))
+                    reDoc(ReDocOptions("/v1/nftkit/redoc").title(title))
                 }))
 
                 val mapper: ObjectMapper = com.fasterxml.jackson.databind.json.JsonMapper.builder()
