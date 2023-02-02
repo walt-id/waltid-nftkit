8db30f2461c50765d04630ddf617f493d3e78f9f
SuperBatata <khaled.battiche@esprit.tn>
2023-02-02 11:51:19 +0100

    create mint NFT Test ✅

--- src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
+++ src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
@@ -12,11 +12,19 @@ class NearProtocol : StringSpec  ({
         result shouldNotBe null
     }
 
-    "deploy smart contract to subaccount with default metadata".config(enabled = enableTest) {
+    "deploy smart contract to subaccount with default metadata".config(enabled = false) {
         val result = NearNftService.deployContractDefault("testingsub.khaled_lightency1.testnet", "testnet")
 
         result shouldNotBe null
     }
+
+    "mint nft".config(enabled = true) {
+        val result = NearNftService.mintNftToken("testingsub.khaled_lightency1.testnet", "testingsub.khaled_lightency1.testnet" , "token1", "testnet token",
+            "token for testing" , "https://images.squarespace-cdn.com/content/v1/609c0ddf94bcc0278a7cbdb4/e73d850a-aad6-419e-84fd-f549ee10ad30/Walt_Logo_Small.png", "", "" , "" , "khaled_lightency1.testnet", "testnet")
+
+        result shouldNotBe null
+    }
+
 
 
 
