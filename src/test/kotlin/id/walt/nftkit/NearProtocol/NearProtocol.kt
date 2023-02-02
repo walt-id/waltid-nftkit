17be6cc70160c597185c4d36b2e2629df18e98aa
SuperBatata <khaled.battiche@esprit.tn>
2023-02-02 11:32:28 +0100

    create deploy contract with default metadata Test ✅

--- src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
+++ src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
@@ -6,8 +6,14 @@ import io.kotest.matchers.shouldNotBe
 
 class NearProtocol : StringSpec  ({
     var enableTest = true
-    "create subaccount".config(enabled = enableTest) {
+    "create subaccount".config(enabled = false) {
         val result = NearNftService.createSubAccount("khaled_lightency1.testnet", "testingsub.khaled_lightency1.testnet", "4" , "testnet")
+
+        result shouldNotBe null
+    }
+
+    "deploy smart contract to subaccount with default metadata".config(enabled = enableTest) {
+        val result = NearNftService.deployContractDefault("testingsub.khaled_lightency1.testnet", "testnet")
 
         result shouldNotBe null
     }
