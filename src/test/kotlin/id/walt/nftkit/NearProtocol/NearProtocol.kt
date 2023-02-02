ed7443f136ef7b5d229f5d5c6f92fb8cac8ad0de
SuperBatata <khaled.battiche@esprit.tn>
2023-02-02 12:09:58 +0100

    Unit tests Done ✅

--- src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
+++ src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
@@ -5,8 +5,8 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldNotBe
 
 class NearProtocol : StringSpec  ({
-    var enableTest = true
-    "create subaccount".config(enabled = false) {
+    val enableTest = false
+    "create sub account".config(enableTest) {
         val result = NearNftService.createSubAccount("khaled_lightency1.testnet", "testingsub.khaled_lightency1.testnet", "4" , "testnet")
 
         result shouldNotBe null
@@ -18,21 +18,21 @@ class NearProtocol : StringSpec  ({
         result shouldNotBe null
     }
 
-    "mint nft".config(enabled = false) {
+    "mint nft".config(enableTest) {
         val result = NearNftService.mintNftToken("testingsub.khaled_lightency1.testnet", "testingsub.khaled_lightency1.testnet" , "token1", "testnet token",
             "token for testing" , "https://images.squarespace-cdn.com/content/v1/609c0ddf94bcc0278a7cbdb4/e73d850a-aad6-419e-84fd-f549ee10ad30/Walt_Logo_Small.png", "", "" , "" , "khaled_lightency1.testnet", "testnet")
 
         result shouldNotBe null
     }
 
-    "Get NFT for owner".config(enabled = false) {
+    "Get NFT for owner".config(enableTest) {
         val result = NearNftService.getNFTforAccount("khaled_lightency1.testnet","testingsub.khaled_lightency1.testnet", "testnet")
         println(result)
         result shouldNotBe null
 
     }
 
-    "Get NFT per token_id".config(enabled = true) {
+    "Get NFT per token_id".config(enableTest) {
         val result = NearNftService.getTokenById("testingsub.khaled_lightency1.testnet","token1", "testnet")
         println(result)
         result shouldNotBe null
