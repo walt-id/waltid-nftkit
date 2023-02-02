d441ca0fd09438fd19ab5626bfe7576e9ae8b7ce
SuperBatata <khaled.battiche@esprit.tn>
2023-02-02 12:06:47 +0100

    create get NFT for owner Test ✅

--- src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
+++ src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
@@ -18,12 +18,21 @@ class NearProtocol : StringSpec  ({
         result shouldNotBe null
     }
 
-    "mint nft".config(enabled = true) {
+    "mint nft".config(enabled = false) {
         val result = NearNftService.mintNftToken("testingsub.khaled_lightency1.testnet", "testingsub.khaled_lightency1.testnet" , "token1", "testnet token",
             "token for testing" , "https://images.squarespace-cdn.com/content/v1/609c0ddf94bcc0278a7cbdb4/e73d850a-aad6-419e-84fd-f549ee10ad30/Walt_Logo_Small.png", "", "" , "" , "khaled_lightency1.testnet", "testnet")
 
         result shouldNotBe null
     }
+
+    "Get NFT for owner".config(enabled = true) {
+        val result = NearNftService.getNFTforAccount("khaled_lightency1.testnet","testingsub.khaled_lightency1.testnet", "testnet")
+        println(result)
+        result shouldNotBe null
+
+    }
+
+
 
 
 
