47595221dea44f139f3b8d0ca45d367a7dd9b0e2
SuperBatata <khaled.battiche@esprit.tn>
2023-02-07 22:28:52 +0100

    unit test for near protocol

--- src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
+++ src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
@@ -7,33 +7,36 @@ import io.kotest.matchers.shouldNotBe
 
 class NearProtocol : StringSpec  ({
     val enableTest = true
+    val random_subaccount = "test" + (20..999).random()
+    print(random_subaccount)
+
     "create sub account".config(enableTest) {
-        val result = NearNftService.createSubAccount("khaled_lightency1.testnet", "testingsub.khaled_lightency1.testnet", "4" , "testnet")
+        val result = NearNftService.createSubAccount("khaled_lightency1.testnet", "${random_subaccount}.khaled_lightency1.testnet", "4" , "testnet")
 
         result shouldNotBe null
     }
 
-    "deploy smart contract to subaccount with default metadata".config(enabled = false) {
-        val result = NearNftService.deployContractDefault("testingsub.khaled_lightency1.testnet", "testnet")
+    "deploy smart contract to subaccount with default metadata".config(enableTest) {
+        val result = NearNftService.deployContractDefault("${random_subaccount}.khaled_lightency1.testnet", "testnet")
 
         result shouldNotBe null
     }
 
     "mint nft".config(enableTest) {
-        val result = NearNftService.mintNftToken("testingsub.khaled_lightency1.testnet", "testingsub.khaled_lightency1.testnet" , "token1", "testnet token",
+        val result = NearNftService.mintNftToken("${random_subaccount}.khaled_lightency1.testnet", "${random_subaccount}.khaled_lightency1.testnet" , "token1", "testnet token",
             "token for testing" , "https://images.squarespace-cdn.com/content/v1/609c0ddf94bcc0278a7cbdb4/e73d850a-aad6-419e-84fd-f549ee10ad30/Walt_Logo_Small.png", "", "" , "" , "khaled_lightency1.testnet", "testnet")
 
         result shouldNotBe null
     }
 
     "Get NFT for owner".config(enableTest) {
-        val result = NearNftService.getNFTforAccount("khaled_lightency1.testnet","testingsub.khaled_lightency1.testnet", "testnet")
+        val result = NearNftService.getNFTforAccount("khaled_lightency1.testnet","${random_subaccount}.khaled_lightency1.testnet", "testnet")
         println(result)
         result shouldNotBe null
     }
 
     "Get NFT per token_id".config(enableTest) {
-        val result = NearNftService.getTokenById("testingsub.khaled_lightency1.testnet","token1", "testnet")
+        val result = NearNftService.getTokenById("${random_subaccount}.khaled_lightency1.testnet","token1", "testnet")
         println(result)
         result shouldNotBe null
     }
