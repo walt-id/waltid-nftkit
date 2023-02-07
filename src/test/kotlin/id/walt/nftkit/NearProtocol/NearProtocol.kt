f720e8a2718762517e52b8b41c035a894fa3e117
SuperBatata <khaled.battiche@esprit.tn>
2023-02-07 21:48:24 +0100

    test should fail again

--- src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
+++ src/test/kotlin/id/walt/nftkit/NearProtocol/NearProtocol.kt
@@ -2,14 +2,15 @@ package id.walt.nftkit.NearProtocol
 
 import id.walt.nftkit.services.NearNftService
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 
 class NearProtocol : StringSpec  ({
     val enableTest = true
     "create sub account".config(enableTest) {
-        val result = NearNftService.createSubAccount("khaled_lightency1.testnet", "testingsub.khaled_lightency1.tenet", "4" , "testnet")
+        val result = NearNftService.createSubAccount("khaled_lightency1.testnet", "testingsub.khaled_lightency1.testnet", "4" , "testnet")
 
-        result shouldNotBe null
+        result shouldBe null
     }
 
     "deploy smart contract to subaccount with default metadata".config(enabled = false) {
