0d106cd2aecc0e8854c8fcfcd30af7d1c28afcc7
Philipp Potisk <phil@walt.id>
2023-02-15 11:57:10 +0100

    ci: New build/release-flow

--- src/main/kotlin/id/walt/nftkit/Values.kt
+++ src/main/kotlin/id/walt/nftkit/Values.kt
@@ -1,7 +1,7 @@
 package id.walt.nftkit
 
 object Values {
-    const val version = "1.0-SNAPSHOT"
+    const val version = "1.SNAPSHOT"
     const val isSnapshot = true
 
     const val POLYGON_MAINNET_CHAIN_ID: Long = 137
