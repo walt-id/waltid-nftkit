f45d813ba6adc5d3c074b3879d45ddc5fc48c2e0
SuperBatata <khaled.battiche@esprit.tn>
2023-03-14 13:06:24 +0100

    update verify signature in NearJs

--- js/near/controllers/near.controller.ts
+++ js/near/controllers/near.controller.ts
@@ -42,6 +42,40 @@ class NearController {
     res.status(201);
     res.send({hash: account });
   }
+  async verifySignature(req: express.Request, res: express.Response) {
+    // try {
+    //
+
+    //   const account = await NearService.verifySignature(
+    //     message,
+    //     req.query.publicKey,
+    //     req.query.signature
+    //   );
+    //   console.log(account);
+    //   res.status(201);
+    // } catch (error) {
+    //   console.log(error);
+    //   res.status(201).send(false);
+    // }
+
+    try {
+      const message = req.query.message;
+      const pk =req.query.publicKey;
+      const sig = req.query.signature;
+
+      console.log(message , pk , sig)
+      const account = await NearService.verifySignature(
+          message,
+          req.query.publicKey,
+          req.query.signature
+      );
+      console.log(account);
+      res.status(201).send(true);
+    } catch (error) {
+      console.log(error);
+      res.status(201).send(false);
+    }
+  }
 }
 
 export default new NearController();
