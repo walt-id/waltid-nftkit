ce06bd72d17470d2f1e0c254bec60b45f2c97e4a
walid <walidelkhemiri@gmail.com>
2023-03-31 14:05:17 +0100

    get NFTs for polkadot unique parachain

--- src/main/kotlin/id/walt/nftkit/models/NFTsInfos.kt
+++ src/main/kotlin/id/walt/nftkit/models/NFTsInfos.kt
@@ -2,11 +2,13 @@ package id.walt.nftkit.models
 
 import id.walt.nftkit.services.NFTsAlchemyResult
 import id.walt.nftkit.services.PolkadotEvmNft
+import id.walt.nftkit.services.PolkadotUniqueNft
 import id.walt.nftkit.services.TezosNFTsTzktResult
 
 class NFTsInfos(
     val evmNfts: List<NFTsAlchemyResult.NftTokenByAlchemy>?= null,
     val tezosNfts: List<TezosNFTsTzktResult>?= null,
-    val polkadotEvmNft: List<PolkadotEvmNft>?= null
+    val polkadotEvmNft: List<PolkadotEvmNft>?= null,
+    val polkadotUniqueNft: List<PolkadotUniqueNft>?= null
 ) {
 }
