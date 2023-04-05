bc8e11a4e14385993d103af114bd514219228763
SuperBatata <khaled.battiche@esprit.tn>
2023-04-05 03:54:22 +0100

    implemented getAllNfts swagger Doc

--- src/main/kotlin/id/walt/nftkit/rest/FlowNftController.kt
+++ src/main/kotlin/id/walt/nftkit/rest/FlowNftController.kt
@@ -1,23 +1,31 @@
 package id.walt.nftkit.rest
 
 import cc.vileda.openapi.dsl.schema
+import id.walt.nftkit.services.FlowNFTMetadata
+import id.walt.nftkit.services.FlowNftService
+import id.walt.nftkit.utilis.Common
 import io.javalin.http.Context
 import io.javalin.plugin.openapi.dsl.document
 
 object FlowNftController {
     fun getAllNFTs(ctx: Context) {
-       // TODO
+        val account_id = ctx.pathParam("account_id")
+        val chain = ctx.pathParam("chain")
+
+        val result = FlowNftService.getAllNFTs(account_id, Common.getFlowChain(chain.lowercase() ))
+        ctx.json(result)
+
+
     }
 
     fun getAllNFTsDocs() = document().operation {
         it.summary("Get NFT token")
-            .operationId("getNftToken").addTagsItem("Flow Blockchain: Non-fungible tokens(NFTs)")
+            .operationId("GetAllNFT").addTagsItem("Flow Blockchain: Non-fungible tokens(NFTs)")
     }.pathParam<String>("chain") {
         it.schema<String> { }
     }
         .pathParam<String>("account_id") {
-        }.pathParam<String>("contract_id") {
-        }.json<String>("200") {
+        }.json<FlowNFTMetadata>("200") {
             it.description("NFT token")
 
         }
