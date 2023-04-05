5196d4f22dc9dc55f03b2c42121e7acc9055a729
SuperBatata <khaled.battiche@esprit.tn>
2023-04-05 03:53:07 +0100

    updated NFT metadata

--- js/Flow/services/flow.service.ts
+++ js/Flow/services/flow.service.ts
@@ -19,6 +19,7 @@ class FlowService {
   async getAllNFTs(Address: string) {
     fcl.config().put("accessNode.api", "https://access-testnet.onflow.org");
     const ad = Address;
+    console.log(ad);
     try {
 
         const response = await fcl.query({
@@ -33,17 +34,17 @@ pub struct NFT {
     pub let description: String
     pub let thumbnail: String
     pub let externalURL: String
-    pub let storagePath: StoragePath
-    pub let publicPath: PublicPath
-    pub let privatePath: PrivatePath
-    pub let publicLinkedType: Type
-    pub let privateLinkedType: Type
+    // pub let storagePath: StoragePath
+    // pub let publicPath: PublicPath
+    // pub let privatePath: PrivatePath
+    // pub let publicLinkedType: Type
+    // pub let privateLinkedType: Type
     pub let collectionName: String
     pub let collectionDescription: String
-    pub let collectionSquareImage: String
-    pub let collectionBannerImage: String
-    pub let collectionExternalURL: String
-    pub let royalties: [MetadataViews.Royalty]
+    // pub let collectionSquareImage: String
+    // pub let collectionBannerImage: String
+    // pub let collectionExternalURL: String
+    // pub let royalties: [MetadataViews.Royalty]
 
     init(
         id: UInt64,
@@ -51,34 +52,34 @@ pub struct NFT {
         description: String,
         thumbnail: String,
         externalURL: String,
-        storagePath: StoragePath,
-        publicPath: PublicPath,
-        privatePath: PrivatePath,
-        publicLinkedType: Type,
-        privateLinkedType: Type,
+        // storagePath: StoragePath,
+        // publicPath: PublicPath,
+        // privatePath: PrivatePath,
+        // publicLinkedType: Type,
+        // privateLinkedType: Type,
         collectionName: String,
         collectionDescription: String,
-        collectionSquareImage: String,
-        collectionBannerImage: String,
-        collectionExternalURL: String,
-        royalties: [MetadataViews.Royalty]
+        // collectionSquareImage: String,
+        // collectionBannerImage: String,
+        // collectionExternalURL: String,
+        // royalties: [MetadataViews.Royalty]
     ) {
         self.id = id
         self.name = name
         self.description = description
         self.thumbnail = thumbnail
         self.externalURL = externalURL
-        self.storagePath = storagePath
-        self.publicPath = publicPath
-        self.privatePath = privatePath
-        self.publicLinkedType = publicLinkedType
-        self.privateLinkedType = privateLinkedType
+        // self.storagePath = storagePath
+        // self.publicPath = publicPath
+        // self.privatePath = privatePath
+        // self.publicLinkedType = publicLinkedType
+        // self.privateLinkedType = privateLinkedType
         self.collectionName = collectionName
         self.collectionDescription = collectionDescription
-        self.collectionSquareImage = collectionSquareImage
-        self.collectionBannerImage = collectionBannerImage
-        self.collectionExternalURL = collectionExternalURL
-        self.royalties = royalties
+        // self.collectionSquareImage = collectionSquareImage
+        // self.collectionBannerImage = collectionBannerImage
+        // self.collectionExternalURL = collectionExternalURL
+        // self.royalties = royalties
     }
 }
 
@@ -126,17 +127,17 @@ pub fun main(ownerAddress: Address): {String: [NFT]} {
                     description: displayView!.description,
                     thumbnail: displayView!.thumbnail.uri(),
                     externalURL: externalURLView!.url,
-                    storagePath: collectionDataView!.storagePath,
-                    publicPath: collectionDataView!.publicPath,
-                    privatePath: collectionDataView!.providerPath,
-                    publicLinkedType: collectionDataView!.publicLinkedType,
-                    privateLinkedType: collectionDataView!.providerLinkedType,
+                    // storagePath: collectionDataView!.storagePath,
+                    // publicPath: collectionDataView!.publicPath,
+                    // privatePath: collectionDataView!.providerPath,
+                    // publicLinkedType: collectionDataView!.publicLinkedType,
+                    // privateLinkedType: collectionDataView!.providerLinkedType,
                     collectionName: collectionDisplayView!.name,
                     collectionDescription: collectionDisplayView!.description,
-                    collectionSquareImage: collectionDisplayView!.squareImage.file.uri(),
-                    collectionBannerImage: collectionDisplayView!.bannerImage.file.uri(),
-                    collectionExternalURL: collectionDisplayView!.externalURL.url,
-                    royalties: royaltyView!.getRoyalties()
+                    // collectionSquareImage: collectionDisplayView!.squareImage.file.uri(),
+                    // collectionBannerImage: collectionDisplayView!.bannerImage.file.uri(),
+                    // collectionExternalURL: collectionDisplayView!.externalURL.url,
+                    // royalties: royaltyView!.getRoyalties()
                 )
             )
         }
@@ -149,7 +150,7 @@ pub fun main(ownerAddress: Address): {String: [NFT]} {
 }
   `,
             //@ts-ignore
-            args: (arg, t) => [arg(ad.Address, t.Address)],
+            args: (arg, t) => [arg(Address, t.Address)],
         });
         return response.example;
     }
