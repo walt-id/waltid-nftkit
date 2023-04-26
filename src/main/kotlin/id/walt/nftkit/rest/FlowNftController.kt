c3c7001babbed678a0bc565ff131230c68a58a63
SuperBatata <khaled.battiche@esprit.tn>
2023-04-26 17:00:20 +0100

    getNFTbyid Controller

--- src/main/kotlin/id/walt/nftkit/rest/FlowNftController.kt
+++ src/main/kotlin/id/walt/nftkit/rest/FlowNftController.kt
@@ -12,21 +12,41 @@ object FlowNftController {
         val account_id = ctx.pathParam("account_id")
         val chain = ctx.pathParam("chain")
 
-        val result = FlowNftService.getAllNFTs(account_id, Common.getFlowChain(chain.lowercase() ))
+        val result = FlowNftService.getAllNFTs(account_id, Common.getFlowChain(chain.lowercase()))
         ctx.json(result)
 
 
     }
 
     fun getAllNFTsDocs() = document().operation {
-        it.summary("Get NFT token")
-            .operationId("GetAllNFT").addTagsItem("Flow Blockchain: Non-fungible tokens(NFTs)")
+        it.summary("Get NFT token").operationId("GetAllNFT").addTagsItem("Flow Blockchain: Non-fungible tokens(NFTs)")
     }.pathParam<String>("chain") {
         it.schema<String> { }
+    }.pathParam<String>("account_id") { }.json<FlowNFTMetadata>("200") {
+        it.description("NFT token")
     }
-        .pathParam<String>("account_id") {
-        }.json<FlowNFTMetadata>("200") {
-            it.description("NFT token")
 
-        }
+    fun getNFTbyId(ctx: Context) {
+        val account_id = ctx.pathParam("account_id")
+        val contractAddress = ctx.pathParam("contractAddress")
+        val contractName = ctx.pathParam("contractName")
+        val token_id = ctx.pathParam("token_id")
+        val chain = ctx.pathParam("chain")
+
+        val result = FlowNftService.getNFTbyId(
+            account_id,
+            contractAddress,
+            contractName,
+            token_id,
+            Common.getFlowChain(chain.lowercase())
+        )
+        ctx.json(result)
+    }
+    fun getNFTbyIdDocs() = document().operation() {
+        it.summary("Get NFT token by id").operationId("GetNFTbyId").addTagsItem("Flow Blockchain: Non-fungible tokens(NFTs)")
+    }.pathParam<String>("chain") {
+        it.schema<String> { }
+    }.pathParam<String>("account_id") { }.pathParam<String>("contractAddress") { }.pathParam<String>("contractName") { }.pathParam<String>("token_id") { }.json<FlowNFTMetadata>("200") {
+        it.description("NFT token")
+    }
 }
