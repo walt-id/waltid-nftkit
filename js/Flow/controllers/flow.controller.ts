ebc7f885612b338cd0133ed662494e9626c23448
SuperBatata <khaled.battiche@esprit.tn>
2023-06-01 13:11:39 +0100

    added verifysignature on FLow controller

--- js/Flow/controllers/flow.controller.ts
+++ js/Flow/controllers/flow.controller.ts
@@ -16,42 +16,69 @@ class FlowController {
     res.send(account);
   }
 
-  async getNFTsByAddressInCollection(req: express.Request, res: express.Response) {
-    const account = await FlowService.getNftsByAddressInCollection(req.body.account_id ,  req.body.collectionPath , req.body.chain)
+  async getNFTsByAddressInCollection(
+    req: express.Request,
+    res: express.Response
+  ) {
+    const account = await FlowService.getNftsByAddressInCollection(
+      req.body.account_id,
+      req.body.collectionPath,
+      req.body.chain
+    );
     console.log(account);
     res.send(account);
   }
 
   async getAllNFTs(req: express.Request, res: express.Response) {
-
-    const account = await FlowService.getAllNFTs(req.body.account_id , req.body.chain)
+    const account = await FlowService.getAllNFTs(
+      req.body.account_id,
+      req.body.chain
+    );
     console.log(account);
     res.send(account);
   }
 
   async getAccountsCollection(req: express.Request, res: express.Response) {
-    const account = await FlowService.getAccountCollection(req.body)
+    const account = await FlowService.getAccountCollection(req.body);
     console.log(account);
     res.send(account);
   }
 
   async getNFTById(req: express.Request, res: express.Response) {
-    const account = await FlowService.getNftById(req.body.account_id ,  req.body.contractAddress , req.body.collectionPublicPath ,req.body.id ,req.body.chain)
+    const account = await FlowService.getNftById(
+      req.body.account_id,
+      req.body.contractAddress,
+      req.body.collectionPublicPath,
+      req.body.id,
+      req.body.chain
+    );
     console.log(account);
     res.send(account);
   }
 
   async getContractName(req: express.Request, res: express.Response) {
-    const account = await FlowService.getContractName()
+    const account = await FlowService.getContractName();
     console.log(account);
   }
 
   async getIDScript(req: express.Request, res: express.Response) {
-    const account = await FlowService.getID_script()
+    const account = await FlowService.getID_script();
     console.log(account);
   }
 
+  async verifySignature(req: express.Request, res: express.Response) {
 
+
+    // @ts-ignore
+    const account = await FlowService.verifySignature(
+        req.query.signature,
+        req.query.message
+
+    );
+
+    res.send(account);
+
+  }
 }
 
 export default new FlowController();
