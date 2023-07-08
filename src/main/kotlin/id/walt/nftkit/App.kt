cf4f6ed431b42fcc6c3171f6d0cdb1f1ff12ffdd
ahmed smiai <ahmedsmiai3@gmail.com>
2023-07-08 23:35:13 +0100

    fetch nft metadata

--- src/main/kotlin/id/walt/nftkit/App.kt
+++ src/main/kotlin/id/walt/nftkit/App.kt
@@ -1,15 +1,26 @@
 package id.walt.nftkit
 
-import id.walt.nftkit.rest.NftKitApi
+import com.algorand.algosdk.v2.client.model.Asset
+import id.walt.nftkit.services.AlgorandChain
+import id.walt.nftkit.services.AlgorandNftService.getAssetMeatadata
+import id.walt.nftkit.services.AlgorandNftService.getNftMetadata
 
 
-fun main() {
+suspend fun main() {
     println("\n\n\n")
-    
+    println(getNftMetadata(	254630815, AlgorandChain.TESTNET))
+
+
+    //            com.algorand.algosdk.v2.client.model.Account acc = client.AccountInformation(src.getAddress()).execute().body();
+//            System.out.println(acc);
+
+//            PostTransactionsResponse txResponse = client.RawTransaction().rawtxn(encodedTxBytes).execute(txHeaders, txValues).body();
+//            System.out.println("Successfully sent tx with ID " + txResponse.txId);
+//
     /* /////////// */
     /* /////////// */
     /* /////////// */
-    NftKitApi.start()
+//    NftKitApi.start()
 
 
 }
