95acdffd8c0959ebbbd92b791e6587eb0343311a
SuperBatata <khaled.battiche@esprit.tn>
2023-07-15 13:48:47 +0100

    update assetcreation service

--- src/main/kotlin/id/walt/nftkit/services/AlgorandNftService.kt
+++ src/main/kotlin/id/walt/nftkit/services/AlgorandNftService.kt
@@ -25,47 +25,41 @@ object AlgorandNftService {
     val token = ""
     val algod = AlgodClient("https://testnet-api.algonode.cloud", 443, token)
     val indexer = IndexerClient("https://testnet-idx.algonode.cloud", 443)
+    val ALGOD_API_ADDR = "https://testnet-algorand.api.purestake.io/ps2"
+    val IDX_API_ADDR = "https://testnet-algorand.api.purestake.io/idx2"
 
-
+    val ALGOD_PORT = 443
+    val ALGOD_API_TOKEN_KEY = "X-API-Key"
+    val ALGOD_API_TOKEN = "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab"
+    val client = AlgodClient(ALGOD_API_ADDR, ALGOD_PORT, ALGOD_API_TOKEN, ALGOD_API_TOKEN_KEY)
+    val idxClient = IndexerClient(IDX_API_ADDR, ALGOD_PORT, ALGOD_API_TOKEN, ALGOD_API_TOKEN_KEY)
     fun createAccount(): AlgorandAccount {
         val account = com.algorand.algosdk.account.Account()
         return AlgorandAccount(account.address.toString(), account.toMnemonic())
     }
 
 
-    fun createAssetArc(){
-        val ALGOD_API_ADDR = "https://testnet-algorand.api.purestake.io/ps2"
-        val IDX_API_ADDR = "https://testnet-algorand.api.purestake.io/idx2"
-
-        val ALGOD_PORT = 443
-        val ALGOD_API_TOKEN_KEY = "X-API-Key"
-        val ALGOD_API_TOKEN = "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab"
-
-        val client = AlgodClient(ALGOD_API_ADDR, ALGOD_PORT, ALGOD_API_TOKEN, ALGOD_API_TOKEN_KEY)
-        val idxClient = IndexerClient(IDX_API_ADDR, ALGOD_PORT, ALGOD_API_TOKEN, ALGOD_API_TOKEN_KEY)
+    fun createAssetArc3(assetName : String , assetUnitName : String ,  url : String) : AlgodResponse{
 
         val SRC_ACCOUNT = "famous hood lend donate orange globe spatial stamp opinion universe found gown river identify negative climb defy galaxy turkey height duty doctor hazard ability athlete"
         val src = Account(SRC_ACCOUNT)
         println(src.getAddress())
 
-        val DEST_ADDR = "ZHGZZQ2PIWYRK6MIK44GKO3VGQUC7NS2V3UQ63M3DIMFUFGI4BRWK7WDBU"
-
         val params = client.TransactionParams().execute().body()
-        val amount = 10L
 
         val tx = Transaction.AssetCreateTransactionBuilder()
             .suggestedParams(params)
-            .sender(src.getAddress())
-            .manager(src.getAddress())
-            .reserve(src.getAddress())
-            .freeze(src.getAddress())
-            .clawback(src.getAddress())
+            .sender(src.address)
+            .manager(src.address)
+            .reserve(src.address)
+            .freeze(src.address)
+            .clawback(src.address)
             .assetTotal(1)
             .assetDecimals(0)
             .defaultFrozen(false)
-            .assetUnitName("ABC")
-            .assetName("ABC")
-            .url("ipfs://bafkreid7pi6nfcv4km3yzsvdfvuldhkuzmlx44rr76uaa7lhir6qmdn47q#arc3")
+            .assetUnitName(assetUnitName)
+            .assetName(assetName)
+            .url("$url#arc3")
             .build()
         val signedTx = src.signTransaction(tx)
 
@@ -76,10 +70,12 @@ object AlgorandNftService {
             val encodedTxBytes = Encoder.encodeToMsgPack(signedTx)
 
             val txResponse = client.RawTransaction().rawtxn(encodedTxBytes).execute(txHeaders, txValues).body()
-            println("Successfully sent tx with ID " + txResponse.txId)
+                println("Successfully sent tx with ID " + txResponse.txId)
+            return AlgodResponse(txResponse.txId , "https://testnet.algoexplorer.io/tx/${txResponse.txId}")
         } catch (e: Exception) {
             e.printStackTrace()
         }
+        return AlgodResponse("Null" , "Null")
     }
 
 }
