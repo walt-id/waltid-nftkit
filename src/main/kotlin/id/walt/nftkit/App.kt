b5a6fc98f1a04514e4cbe6a537b3cf56b054d18f
SuperBatata <khaled.battiche@esprit.tn>
2023-07-16 00:06:27 +0100

    clean App.kt

--- src/main/kotlin/id/walt/nftkit/App.kt
+++ src/main/kotlin/id/walt/nftkit/App.kt
@@ -1,25 +1,11 @@
 package id.walt.nftkit
 
-import com.algorand.algosdk.v2.client.model.Asset
+
 import id.walt.nftkit.rest.NftKitApi
-import id.walt.nftkit.services.AlgoNftMetadata
-import id.walt.nftkit.services.AlgorandChain
-import id.walt.nftkit.services.AlgorandNftService.getAssetMeatadata
-import id.walt.nftkit.services.AlgorandNftService.getNftMetadata
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 
 
 fun main() {
     println("\n\n\n")
-    val test = getNftMetadata(254630815, AlgorandChain.TESTNET)
-    println(test)
-    //            com.algorand.algosdk.v2.client.model.Account acc = client.AccountInformation(src.getAddress()).execute().body();
-//            System.out.println(acc);
-
-//            PostTransactionsResponse txResponse = client.RawTransaction().rawtxn(encodedTxBytes).execute(txHeaders, txValues).body();
-//            System.out.println("Successfully sent tx with ID " + txResponse.txId);
-//
     /* /////////// */
     /* /////////// */
     /* /////////// */
