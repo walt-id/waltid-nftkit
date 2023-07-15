c791a024c00d92a14fe1ba0e224daed8c6e60f04
SuperBatata <khaled.battiche@esprit.tn>
2023-07-16 00:08:39 +0100

    fixed endpoint swagger tags for algorand

--- src/main/kotlin/id/walt/nftkit/rest/AlgorandNftController.kt
+++ src/main/kotlin/id/walt/nftkit/rest/AlgorandNftController.kt
@@ -1,16 +1,16 @@
 package id.walt.nftkit.rest
 
 import cc.vileda.openapi.dsl.schema
-import id.walt.nftkit.services.AlgodResponse
-import id.walt.nftkit.services.AlgorandAccount
-import id.walt.nftkit.services.AlgorandChain
-import id.walt.nftkit.services.AlgorandNftService
+
+import id.walt.nftkit.services.*
 import id.walt.nftkit.tokenownersquery.TokenOwnersDataResponse
 import id.walt.nftkit.utilis.Common
 import io.javalin.http.Context
 import io.javalin.plugin.openapi.dsl.document
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+
+
 
 object AlgorandNftController{
     fun accountCreation(ctx : Context){
@@ -37,7 +37,7 @@ object AlgorandNftController{
     }
 
 
-    private const val TAG = "Algorand Blockchain"
+
     fun fetchAssetMetadata(ctx: Context){
         val chain =ctx.pathParam("chain")
         val asset  = ctx.pathParam("asset")
@@ -51,7 +51,7 @@ object AlgorandNftController{
     fun fetchAssetMetadataDocs()= document().operation {
         it.summary("Fetching Asset on Algorand Network")
             .operationId("fetchAlgornadAssets")
-            .addTagsItem(TAG)}
+            .addTagsItem("Algorand Blockchain: Non-fungible tokens(NFTs)")}
         .pathParam<String>("chain") {
             it.schema<AlgorandChain>{}}
         .pathParam<String>("asset"){}
@@ -71,11 +71,11 @@ object AlgorandNftController{
     fun fetchNftMetadataDocs()= document().operation {
         it.summary("Fetching NFTs on Algorand Network")
             .operationId("fetchAlgornadNfts")
-            .addTagsItem(TAG)}
+            .addTagsItem("Algorand Blockchain: Non-fungible tokens(NFTs)")}
         .pathParam<String>("chain") {
             it.schema<AlgorandChain>{}}
         .pathParam<String>("asset"){}
-        .json<TokenOwnersDataResponse>("200"){
+        .json<String>("200"){
             it.description("Fetched NFT")
         }
 
