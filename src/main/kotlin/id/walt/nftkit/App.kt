5e6c1f3309c2803cc0c69a08fcd1796b6124e92a
ahmedsmiai <47064156+ahmedsmiai@users.noreply.github.com>
2023-07-17 01:30:43 +0100

    Update App.kt

--- src/main/kotlin/id/walt/nftkit/App.kt
+++ src/main/kotlin/id/walt/nftkit/App.kt
@@ -14,14 +14,7 @@ import kotlinx.serialization.json.Json
 
 fun main() {
     println("\n\n\n")
-    val test = getToken(257170593, AlgorandChain.TESTNET)
-    println(test)
-    //            com.algorand.algosdk.v2.client.model.Account acc = client.AccountInformation(src.getAddress()).execute().body();
-//            System.out.println(acc);
-
-//            PostTransactionsResponse txResponse = client.RawTransaction().rawtxn(encodedTxBytes).execute(txHeaders, txValues).body();
-//            System.out.println("Successfully sent tx with ID " + txResponse.txId);
-//
+    
     /* /////////// */
     /* /////////// */
     /* /////////// */
