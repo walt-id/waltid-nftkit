c9b7257f566ee2b1e79c364a43165915ad658a1e
SuperBatata <khaled.battiche@esprit.tn>
2023-07-29 14:42:42 +0100

    clean app.kt

--- src/main/kotlin/id/walt/nftkit/App.kt
+++ src/main/kotlin/id/walt/nftkit/App.kt
@@ -1,15 +1,7 @@
 package id.walt.nftkit
 
 
-import com.beust.klaxon.JsonObject
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import id.walt.nftkit.rest.NftKitApi
-import id.walt.nftkit.services.AlgorandChain
-import id.walt.nftkit.services.AlgorandNftService
-import id.walt.nftkit.services.NftService
-import id.walt.nftkit.services.OffChainMetadataStorageType
-
-
 
 fun main() {
 
