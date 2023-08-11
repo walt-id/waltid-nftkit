6fbee471521a3d9257e1e59257f840b17a1f7ff9
SuperBatata <khaled.battiche@esprit.tn>
2023-08-11 16:08:09 +0100

    algorand sig verification service

--- js/algorand/services/algorand.service.ts
+++ js/algorand/services/algorand.service.ts
@@ -1,3 +1,21 @@
+import * as dotenv from "dotenv";
+import algosdk from "algosdk";
+
+
+dotenv.config();
 class AlgorandService{
-//TODO
+
+
+    async verifySignature(message: string, signature: string, address: string) {
+
+        const _message = new Uint8Array(Buffer.from(message));
+        const _signature = new Uint8Array(signature.split(',').map(Number))
+
+        // @ts-ignore
+        return algosdk.verifyBytes(_message, _signature, address);
+
+    }
 }
+
+
+export default new AlgorandService();
