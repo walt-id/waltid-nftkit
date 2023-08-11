48753857f29ff662f513950897f841fb5e8b72f7
SuperBatata <khaled.battiche@esprit.tn>
2023-08-11 16:07:14 +0100

    verify signature function for algorand

--- js/algorand/controllers/algorand.controller.ts
+++ js/algorand/controllers/algorand.controller.ts
@@ -1,4 +1,23 @@
+import express from "express";
+import AlgorandService from "../services/algorand.service";
+
+
+
+
 class AlgorandController {
 
-//TODO
+
+    async verifySignature(req: express.Request, res: express.Response) {
+
+        const account = await AlgorandService.verifySignature(
+            //@ts-ignore
+            req.query.message,
+            req.query.signature,
+            req.query.publicKey
+        );
+        console.log(account);
+        res.status(201).send(account);
+    }
+
 }
+export default new AlgorandController();
