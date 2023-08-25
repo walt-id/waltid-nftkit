3f718183d91202c46c6e93e644a941f8a20e097b
SuperBatata <khaled.battiche@esprit.tn>
2023-08-25 01:05:24 +0100

    fix: load soulbound contract function

--- src/main/kotlin/id/walt/nftkit/chains/evm/erc721/SoulBoundTokenStandard.kt
+++ src/main/kotlin/id/walt/nftkit/chains/evm/erc721/SoulBoundTokenStandard.kt
@@ -5,10 +5,6 @@ import id.walt.nftkit.WaltIdGasProvider
 import id.walt.nftkit.services.*
 import id.walt.nftkit.utilis.providers.ProviderFactory
 import org.web3j.abi.datatypes.Address
-import org.web3j.abi.datatypes.Bool
-import org.web3j.abi.datatypes.DynamicBytes
-import org.web3j.abi.datatypes.Utf8String
-import org.web3j.abi.datatypes.generated.Bytes4
 import org.web3j.abi.datatypes.generated.Uint256
 import org.web3j.crypto.Credentials
 import org.web3j.protocol.core.RemoteCall
@@ -16,47 +12,46 @@ import org.web3j.protocol.core.methods.response.TransactionReceipt
 import org.web3j.tx.RawTransactionManager
 import org.web3j.tx.TransactionManager
 import org.web3j.tx.gas.ContractGasProvider
-import org.web3j.utils.Numeric
-import smart_contract_wrapper.CustomAccessControlERC721
 import smart_contract_wrapper.CustomOwnableERC721
+import smart_contract_wrapper.SoulBoundTest
 import java.math.BigInteger
 
-object SoulBoundTokenStandard :  IErc721TokenStandard  {
+object SoulBoundTokenStandard :  ISoulBoundTokenStandard  {
 
 
-    fun deployContract(chain: EVMChain, parameter: DeploymentParameter, options: DeploymentOptions): DeploymentResponse {
-        /*val web3j = ProviderFactory.getProvider(chain)?.getWeb3j()
-        val credentials: Credentials = Credentials.create(WaltIdServices.loadChainConfig().privateKey)
+
+
+
+
+    private fun loadContract(chain: EVMChain, address: String, signedAccount: String? ="") : SoulBoundTest {
+        val web3j = ProviderFactory.getProvider(chain)?.getWeb3j()
+
+        val privateKey: String = if(signedAccount == null || "" == (signedAccount)){
+            WaltIdServices.loadChainConfig().privateKey
+        }else{
+            val lowercaseAddress= WaltIdServices.loadAccountKeysConfig().keys.mapKeys { it.key.lowercase() }
+            lowercaseAddress[signedAccount.lowercase()]!!
+        }
+
+        val credentials: Credentials = Credentials.create(privateKey)
+
         val gasProvider: ContractGasProvider = WaltIdGasProvider
-        val remotCall : RemoteCall<ERC721URIStorage>
-        if(chain == Chain.POLYGON || chain == Chain.MUMBAI){
-            val chainId : Long
-            if(chain == Chain.POLYGON){
-                chainId = Values.POLYGON_MAINNET_CHAIN_ID
-            }else{
-                chainId = Values.POLYGON_TESTNET_MUMBAI_CHAIN_ID
-            }
-            val transactionManager: TransactionManager = RawTransactionManager(
-                web3j, credentials, chainId
-            )
-            remotCall = ERC721URIStorage.deploy(web3j,transactionManager,gasProvider,Utf8String(parameter.name),Utf8String(parameter.symbol))
-        }else{
-            remotCall = ERC721URIStorage.deploy(web3j,credentials,gasProvider,Utf8String(parameter.name),Utf8String(parameter.symbol))
+        val chainId= when(chain){
+            EVMChain.ETHEREUM -> Values.ETHEREUM_MAINNET_CHAIN_ID
+            EVMChain.GOERLI -> Values.ETHEREUM_TESTNET_GOERLI_CHAIN_ID
+            EVMChain.SEPOLIA -> Values.ETHEREUM_TESTNET_SEPOLIA_CHAIN_ID
+            EVMChain.POLYGON -> Values.POLYGON_MAINNET_CHAIN_ID
+            EVMChain.MUMBAI -> Values.POLYGON_TESTNET_MUMBAI_CHAIN_ID
+            EVMChain.ASTAR -> Values.ASTAR_MAINNET_CHAIN_ID
+            EVMChain.MOONBEAM -> Values.MOONBEAM_MAINNET_CHAIN_ID
+            EVMChain.SHIMMEREVM -> Values.SHIMMEREVM_TESTNET_CHAIN_ID
         }
-        val contract= remotCall.send()
+        val transactionManager: TransactionManager = RawTransactionManager(
+            web3j, credentials, chainId
+        )
+        return  SoulBoundTest.load(address, web3j,transactionManager,gasProvider)
 
-        val url = WaltIdServices.getBlockExplorerUrl(chain)
-        val ts = TransactionResponse(contract.transactionReceipt.get().transactionHash,  "$url/tx/${contract.transactionReceipt.get().transactionHash}" )
-        return DeploymentResponse(ts, contract.contractAddress, "$url/address/${contract.contractAddress}" )*/
-        /*if(options.accessControl == AccessControl.OWNABLE){
-            return deployOwnableContract(chain, parameter, options)
-        }else{
-            return deployRBACContract(chain, parameter, options)
-        }*/
-        return when (options.accessControl) {
-            AccessControl.OWNABLE -> deployOwnableContract(chain, parameter, options)
-            AccessControl.ROLE_BASED_ACCESS_CONTROL -> deployRBACContract(chain, parameter, options)
-        }
+    }
 
     }
     override fun mintToken(
