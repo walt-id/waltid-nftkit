a7b06d8ead803db1d219e89f3878adc327e6fb8e
SuperBatata <khaled.battiche@esprit.tn>
2023-08-25 01:09:18 +0100

    refactor : ISoulBoundTokenStandard

--- src/main/kotlin/id/walt/nftkit/chains/evm/erc721/ISoulBoundTokenStandard.kt
+++ src/main/kotlin/id/walt/nftkit/chains/evm/erc721/ISoulBoundTokenStandard.kt
@@ -6,19 +6,13 @@ import org.web3j.abi.datatypes.Bool
 import org.web3j.abi.datatypes.DynamicBytes
 import org.web3j.abi.datatypes.Utf8String
 import org.web3j.abi.datatypes.generated.Uint256
+import org.web3j.protocol.core.RemoteFunctionCall
 import org.web3j.protocol.core.methods.response.TransactionReceipt
 import java.math.BigInteger
 interface ISoulBoundTokenStandard {
 
-    fun mintToken(
-        chain: EVMChain,
-        contractAddress: String,
-        recipient: Address,
-        tokenURI: Utf8String
-    ): TransactionReceipt?
-
-
-    fun ownerOf(chain: EVMChain, contractAddress: String, tokenId: Uint256): String?
+    fun safeMint(chain: EVMChain, contractAddress: String,to: String, uri: String): TransactionReceipt?
+    fun ownerOf(chain: EVMChain, contractAddress: String, tokenId: Uint256):String?
 
     fun name(chain: EVMChain, contractAddress: String): String?
 
@@ -28,16 +22,7 @@ interface ISoulBoundTokenStandard {
 
     fun balanceOf(chain: EVMChain, contractAddress: String, owner: Address): BigInteger?
 
-    fun supportsInterface(chain: EVMChain, contractAddress: String): Boolean
-
-    fun updateTokenUri(chain: EVMChain, contractAddress: String, token: BigInteger, tokenURI: Utf8String, signedAccount: String?) : TransactionReceipt?
-
-    fun transferFrom(chain: EVMChain, contractAddress: String, from: Address, to: Address, tokenId: Uint256, signedAccount: String?): TransactionReceipt
     fun safeTransferFrom(chain: EVMChain, contractAddress: String, from: Address, to: Address, tokenId: Uint256, signedAccount: String?): TransactionReceipt
-    fun safeTransferFrom(chain: EVMChain, contractAddress: String, from: Address, to: Address, tokenId: Uint256, data: DynamicBytes, signedAccount: String?): TransactionReceipt
-    fun setApprovalForAll(chain: EVMChain, contractAddress: String, operator: Address, approved: Bool, signedAccount: String?): TransactionReceipt
-    fun isApprovedForAll(chain: EVMChain, contractAddress: String, owner: Address, operator: Address): Bool
-    fun approve(chain: EVMChain, contractAddress: String, to: Address, tokenId: Uint256, signedAccount: String?):TransactionReceipt
-    fun getApproved(chain: EVMChain, contractAddress: String, tokenId: Uint256): Address
+
 
 }
